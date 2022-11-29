146fd94999c129e37ab1a2e1c55bd89db21cede0
tsmadzharov21 <tsmadzharov21@codingburgas.bg>
2022-11-29 23:00:53 +0200

    Made Headers

---
+++ game/hangman/mainMenu.h
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void mainMenu()
+{
+	cout << "Welcome to hanggirl!" << endl;
+	cout << "If you want to play the game type: Y" << endl;
+	cout << "If you want to quit type: N" << endl;
+	cout << "Type your answer:";
+}

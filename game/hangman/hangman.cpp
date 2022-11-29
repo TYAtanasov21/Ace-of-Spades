e167750d2d9c5bd6130fa16bb72599faaada3f62
Todor Atanasov <TYAtanasov21@codingburgas.bg>
2022-11-29 19:39:09 +0200

    Add main Menu

--- game/hangman/hangman.cpp
+++ game/hangman/hangman.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
-
 using namespace std;
+void yesOrNo()
+{
+	char input;
+	cin >> input;
+	if (input == 'N')
+	{
+		cout << "Thank you for playing!";
+	}
+	if (input == 'Y')
+	{
+		cout << "You may continue!";
+	}
+	else if (input != 'N' && input != 'Y')
+	{
+		cout << "Incorrect input!" << endl;
+		cout << "Please choose another option!" << endl;
+		yesOrNo();
+	}
+}
+void mainMenu()
+{
+	cout << "Welcome to hanggirl!" << endl;
+	cout << "If you want to play the game type: Y" << endl;
+	cout << "If you want to quit type: N" << endl;
+	cout << "Type your answear:";
+	yesOrNo();
+}
 int main()
 {
-	cout << "test";
+	mainMenu();
+	return 0;
 }

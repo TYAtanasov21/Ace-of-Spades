37ea9ae87db6bbcb1a8285bb3f3f62b9c89c4c75
tsmadzharov21 <107103896+tsmadzharov21@users.noreply.github.com>
2022-12-03 16:31:35 +0200

    Delete mainMenu.h

--- game/hangman/mainMenu.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-void mainMenu()
-{
-	cout << "Welcome to hanggirl!" << endl;
-	cout << "If you want to play the game type: Y" << endl;
-	cout << "If you want to quit type: N" << endl;
-	cout << "Type your answer:";
-}
